ab186df261e2c8ad7f6dc20332e178034bdccf45
javier101195 <javiermendozagonzalez@gmail.com>
2018-12-09 21:06:21 -0500

    Guardar un archivo de texto en la memoria interna

--- app/src/main/java/com/example/javier/grabarmemoria/MainActivity.java
+++ app/src/main/java/com/example/javier/grabarmemoria/MainActivity.java
@@ -1,86 +1,80 @@
 package com.example.javier.grabarmemoria;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import android.os.Environment;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText et1, et2;
-    Button button1, button2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = (EditText) findViewById(R.id.editText1);
-        et2 = (EditText) findViewById(R.id.editText2);
-        button1=(Button) findViewById(R.id.button);
-        button2=(Button) findViewById(R.id.button2);
+        et1 = (EditText) findViewById(R.id.et1);
+        et2 = (EditText) findViewById(R.id.et2);
+
 
 
     }
-
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }*/
 
 
     public void grabar(View v) {
-        String nomarchivo = et1.getText().toString();
-        String contenido = et2.getText().toString();
+        String nomarchivo=et1.getText().toString();
+        nomarchivo=nomarchivo.replace('/','-');
         try {
-            File tarjeta = Environment.getExternalStorageDirectory();
-            File file = new File(tarjeta.getAbsolutePath(), nomarchivo);
-            OutputStreamWriter osw = new OutputStreamWriter(
-                    new FileOutputStream(file));
-            osw.write(contenido);
-            osw.flush();
-            osw.close();
-            Toast.makeText(this, "Los datos fueron grabados correctamente",
-                    Toast.LENGTH_SHORT).show();
-            et1.setText("");
-            et2.setText("");
-        } catch (IOException ioe) {
+            OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput(
+                    nomarchivo, Activity.MODE_PRIVATE));
+            archivo.write(et2.getText().toString());
+            archivo.flush();
+            archivo.close();
+        } catch (IOException e) {
         }
+        Toast t = Toast.makeText(this, "Los datos fueron grabados",
+                Toast.LENGTH_SHORT);
+        t.show();
+        et1.setText("");
+        et2.setText("");
     }
 
-
     public void recuperar(View v) {
-        String nomarchivo = et1.getText().toString();
-        File tarjeta = Environment.getExternalStorageDirectory();
-        File file = new File(tarjeta.getAbsolutePath(), nomarchivo);
-        try {
-            FileInputStream fIn = new FileInputStream(file);
-            InputStreamReader archivo = new InputStreamReader(fIn);
-            BufferedReader br = new BufferedReader(archivo);
-            String linea = br.readLine();
-            String todo = "";
-            while (linea != null) {
-                todo = todo + linea + "";
-                linea = br.readLine();
+        String nomarchivo=et1.getText().toString();
+        nomarchivo=nomarchivo.replace('/','-');
+        boolean enco=false;
+        String[] archivos = fileList();
+        for (int f = 0; f < archivos.length; f++)
+            if (nomarchivo.equals(archivos[f]))
+                enco= true;
+        if (enco==true) {
+            try {
+                InputStreamReader archivo = new InputStreamReader(
+                        openFileInput(nomarchivo));
+                BufferedReader br = new BufferedReader(archivo);
+                String linea = br.readLine();
+                String todo = "";
+                while (linea != null) {
+                    todo = todo + linea + "\n";
+                    linea = br.readLine();
+                }
+                br.close();
+                archivo.close();
+                et2.setText(todo);
+            } catch (IOException e) {
             }
-            br.close();
-            archivo.close();
-            et2.setText(todo);
-
-        } catch (IOException e) {
+        } else
+        {
+            Toast.makeText(this,"No hay datos grabados para dicha fecha", Toast.LENGTH_LONG).show();
+            et2.setText("");
         }
     }
 }
